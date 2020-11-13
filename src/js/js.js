e6c420733ce1a27466716ad9f80c9468b36bc7fe
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-11-13 12:36:01 -0400

    evitando colisao top e bot

--- src/js/js.js
+++ src/js/js.js
@@ -49,10 +49,16 @@ function start() { // Inicio da função start()
   } // fim da função movefundo()
 
   function movejogador() {
-	
+
     if (jogo.pressionou[TECLA.W]) {
       var topo = parseInt($("#jogador").css("top"));
-      $("#jogador").css("top",topo-10);
+      
+      if (!(topo<=0)) {
+        
+        $("#jogador").css("top",topo-10);
+        //$("#jogador").css("top",topo+10);
+      }
+      
     
     }
     
@@ -60,8 +66,13 @@ function start() { // Inicio da função start()
       
       var topo = parseInt($("#jogador").css("top"));
       $("#jogador").css("top",topo+10);	
+
+      if (topo>=434) {	
+        $("#jogador").css("top",topo-10);
+          
+      }
     }
-    
+      
     if (jogo.pressionou[TECLA.D]) {
       
       //Chama função Disparo	
