d3b068b2e4bb46bdeb04cdc2024aa11e0486adb8
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-11-13 12:27:04 -0400

    mover jogador

--- src/js/js.js
+++ src/js/js.js
@@ -8,6 +8,25 @@ function start() { // Inicio da função start()
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
   
   var jogo = {}
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+    }
+  
+  jogo.pressionou = [];
+
+  //Verifica se o usuário pressionou alguma tecla	
+	
+	$(document).keydown(function(e){
+    jogo.pressionou[e.which] = true;
+    });
+  
+  
+  $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+  });
+  
 	
 	//Game Loop
 
@@ -15,7 +34,8 @@ function start() { // Inicio da função start()
 	
 	function loop() {
 	
-	  movefundo();
+    movefundo();
+    movejogador();
 	
   } // Fim da função loop()
   
@@ -26,6 +46,27 @@ function start() { // Inicio da função start()
     esquerda = parseInt($("#fundoGame").css("background-position"));
     $("#fundoGame").css("background-position",esquerda-1);
     
-    } // fim da função movefundo()
+  } // fim da função movefundo()
+
+  function movejogador() {
+	
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo-10);
+    
+    }
+    
+    if (jogo.pressionou[TECLA.S]) {
+      
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top",topo+10);	
+    }
+    
+    if (jogo.pressionou[TECLA.D]) {
+      
+      //Chama função Disparo	
+    }
+  
+    } // fim da função movejogador()
 
 } // Fim da função start
