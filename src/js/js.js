d9673dbcc56c59e4646749c5229c8b84c430c8c2
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-11-18 13:12:06 -0400

    placar do jogo

--- src/js/js.js
+++ src/js/js.js
@@ -7,10 +7,15 @@ function start() {
   $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
   $("#fundoGame").append("<div id='inimigo2'></div>");
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+  $("#fundoGame").append("<div id='placar'></div>");
+
 
   //principais variaveis do jogo
   var podeAtirar = true;
   var fimdejogo = false;
+  var pontos=0;
+  var salvos=0;
+  var perdidos=0;
   var jogo = {};
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -43,6 +48,8 @@ function start() {
     moveinimigo2();
     moveamigo();
     colisao();
+    placar();
+
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -168,6 +175,7 @@ function start() {
     // Disparo com o inimigo1
 
     if (colisao3.length > 0) {
+      pontos=pontos+100;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
 
@@ -182,6 +190,8 @@ function start() {
     // Disparo com o inimigo2
 
     if (colisao4.length > 0) {
+      pontos=pontos+50;
+
       inimigo2X = parseInt($("#inimigo2").css("left"));
       inimigo2Y = parseInt($("#inimigo2").css("top"));
       $("#inimigo2").remove();
@@ -195,6 +205,7 @@ function start() {
     // jogador com o amigo
 
     if (colisao5.length > 0) {
+      salvos++;
       reposicionaAmigo();
       $("#amigo").remove();
     }
@@ -202,6 +213,7 @@ function start() {
     //Inimigo2 com o amigo
 
     if (colisao6.length > 0) {
+      perdidos++;
       amigoX = parseInt($("#amigo").css("left"));
       amigoY = parseInt($("#amigo").css("top"));
       explosao3(amigoX, amigoY);
@@ -294,6 +306,13 @@ function explosao3(amigoX,amigoY) {
       }
     }
   } // Fim da função reposicionaAmigo()
+
+  function placar() {
+	
+    $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
+    
+  } //fim da função placar()
+  
 
 
 } // Fim da função start
