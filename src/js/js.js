64efc69a6b82a812383fd4a644bb6a53820aff1b
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-11-18 13:17:52 -0400

    barra de energia

--- src/js/js.js
+++ src/js/js.js
@@ -8,6 +8,7 @@ function start() {
   $("#fundoGame").append("<div id='inimigo2'></div>");
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
   $("#fundoGame").append("<div id='placar'></div>");
+  $("#fundoGame").append("<div id='energia'></div>");
 
 
   //principais variaveis do jogo
@@ -18,6 +19,7 @@ function start() {
   var perdidos=0;
   var jogo = {};
   var velocidade = 5;
+  var energiaAtual=3;
   var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
     W: 87,
@@ -49,6 +51,7 @@ function start() {
     moveamigo();
     colisao();
     placar();
+    energia();
 
   } // Fim da função loop()
 
@@ -152,6 +155,7 @@ function start() {
     // jogador com o inimigo1
 
     if (colisao1.length > 0) {
+      energiaAtual--;
       inimigo1X = parseInt($("#inimigo1").css("left"));
       inimigo1Y = parseInt($("#inimigo1").css("top"));
       explosao1(inimigo1X, inimigo1Y);
@@ -163,6 +167,7 @@ function start() {
 
     // jogador com o inimigo2
     if (colisao2.length > 0) {
+      energiaAtual--;
       inimigo2X = parseInt($("#inimigo2").css("left"));
       inimigo2Y = parseInt($("#inimigo2").css("top"));
       explosao2(inimigo2X, inimigo2Y);
@@ -312,6 +317,34 @@ function explosao3(amigoX,amigoY) {
     $("#placar").html("<h2> Pontos: " + pontos + " Salvos: " + salvos + " Perdidos: " + perdidos + "</h2>");
     
   } //fim da função placar()
+
+  //Barra de energia
+
+  function energia() {
+    
+    if (energiaAtual==3) {
+      
+      $("#energia").css("background-image", "url(src/imgs/energia3.png)");
+    }
+
+    if (energiaAtual==2) {
+      
+      $("#energia").css("background-image", "url(src/imgs/energia2.png)");
+    }
+
+    if (energiaAtual==1) {
+      
+      $("#energia").css("background-image", "url(src/imgs/energia1.png)");
+    }
+
+    if (energiaAtual==0) {
+      
+      $("#energia").css("background-image", "url(src/imgs/energia0.png)");
+      
+      //Game Over
+    }
+
+} // Fim da função energia()
   
 
 
